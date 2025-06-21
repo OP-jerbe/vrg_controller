308b1edf17a2436d1e05e0a3876c56c83d9a9385
fizziksfool <erbe.joshua@gmail.com>
2025-06-21 07:36:46 -0700

    Adds the icon to the window

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -1,7 +1,25 @@
+from pathlib import Path
+
+from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QMainWindow
+
+from helpers.helpers import get_root_dir
 
 
 class MainWindow(QMainWindow):
     def __init__(self, version: str) -> None:
         super().__init__()
         self.version = version
+
+        self.create_gui()
+
+    def create_gui(self) -> None:
+        window_width = 500
+        window_height = 300
+        self.setFixedSize(window_width, window_height)
+
+        root_dir: Path = get_root_dir()
+        print(str(root_dir))
+        icon_path: str = str(root_dir / 'assets' / 'vrg_icon.ico')
+        self.setWindowIcon(QIcon(icon_path))
+        self.setWindowTitle('VRG Controller')
