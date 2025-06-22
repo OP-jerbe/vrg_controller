fe2bab88a74b2c4508b133e15893092e7d7f4a13
fizziksfool <erbe.joshua@gmail.com>
2025-06-22 16:25:47 -0700

    Adds the ini_reader.py file

---
+++ src/ini_reader.py
@@ -0,0 +1,42 @@
+import configparser
+from typing import TypeAlias
+
+ConfigData: TypeAlias = configparser.ConfigParser
+
+
+def load_config(file_name: str) -> ConfigData:
+    config_data = configparser.ConfigParser()
+    config_data.read(file_name)
+    return config_data
+
+
+def find_comport_device(config_data: ConfigData, header: str) -> tuple[str, str]:
+    device = config_data.get(header, 'device')
+    address = config_data.get(header, 'com_port')
+    return device, address
+
+
+def get_rf_settings(
+    config_data: ConfigData, header: str = 'RFSettings'
+) -> tuple[str, str, str]:
+    min_freq = config_data.get(header, 'min_freq')
+    max_freq = config_data.get(header, 'max_freq')
+    max_power = config_data.get(header, 'max_power')
+    return min_freq, max_freq, max_power
+
+
+if __name__ == '__main__':
+    ini_file = 'rf_controller.ini'
+
+    config_data = load_config(ini_file)
+    rf_generator = find_comport_device(config_data, 'RFGenerator')
+    vrg_settings = get_rf_settings(config_data)
+
+    rf_device, rf_com_port = rf_generator
+    min_freq, max_freq, max_power = vrg_settings
+
+    print(f'{rf_device = }')
+    print(f'{rf_com_port = }')
+    print(f'{min_freq = } MHz')
+    print(f'{max_freq = } MHz')
+    print(f'{max_power = } W')
